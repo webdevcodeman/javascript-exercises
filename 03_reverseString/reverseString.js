896ca80e11b1d670714ebdf4aef9fce2e862f040
webdev <117325718+webdevcodeman@users.noreply.github.com>
2023-01-21 17:46:19 +0530

    Reverse the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(oStr) {
+  // let rStr="";
+  // for(let i=oStr.length-1;i>=0;i--){
+  //   rStr+=oStr[i];
+  // }
+  // return rStr;
+  return oStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
