7fd042999293b6db30ec8b8e6d9608264458ff3e
webdev <117325718+webdevcodeman@users.noreply.github.com>
2023-01-21 16:53:06 +0530

    Return a string repeated "n" times, if n<0 "ERROR"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+  let retString="";
+  if(num<0){
+    return "ERROR";
+  }
+  for(let i=0;i<num;i++){
+    retString+=string;
+  }
+  return retString;
 };
 
 // Do not edit below this line
