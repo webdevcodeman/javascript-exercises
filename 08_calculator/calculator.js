da99d7864d24d7cdf09767caaae957a3a4ff7094
webdev <117325718+webdevcodeman@users.noreply.github.com>
2023-01-22 19:30:52 +0530

    Create add, subt, sum & multip of arr, power, fact

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let arrSum=0;
+	for(let i=0;i<arr.length;i++){
+    arrSum+=arr[i];
+  }
+  return arrSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let arrProduct=1;
+  for(let i=0;i<arr.length;i++){
+    arrProduct*=arr[i];
+  }
+  return arrProduct;
 };
 
-const power = function() {
-	
+const power = function(base,pow) {
+	return base**pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  // let fact=1;
+	// if(num===0){
+  //   return fact;
+  // }
+  // for(let i=num;i>1;i--){
+  //   fact*=i;
+  // }
+  // return fact;
+  if(num===0 || num===1){
+    return 1;
+  }else{
+    return num*factorial(num-1);
+  }
 };
 
 // Do not edit below this line
