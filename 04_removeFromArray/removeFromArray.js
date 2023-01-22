69bd8c2e071a12eb7ab6bf80d693dcebcc8cbc8d
webdev <117325718+webdevcodeman@users.noreply.github.com>
2023-01-22 15:50:58 +0530

    Return array of removed elements (diff. approach)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,19 @@
 const removeFromArray = function(arr,...args) {
-  for(const arg of args){
-    const index=arr.indexOf(arg);
-    if(index!==-1){
-      arr.splice(index,1);
-    }
-  }
-  return arr;
+  // for(const arg of args){
+  //   const index=arr.indexOf(arg);
+  //   if(index!==-1){
+  //     arr.splice(index,1);
+  //   }
+  // }
+  // return arr;
+  // let newArr=[];
+  // arr.forEach((item)=>{
+  //   if(!args.includes(item)){
+  //     newArr.push(item);
+  //   }
+  // });
+  // return newArr;
+  return arr.filter((val)=>{return !args.includes(val);});
 };
 
 // Do not edit below this line
