72731cad84f2183b7c9b1037cd6e41760b23ad8c
webdev <117325718+webdevcodeman@users.noreply.github.com>
2023-01-23 16:28:35 +0530

    Check whether given string is a palindrome or not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // let newStr="";
+  // const lowerStr=str.toLowerCase();
+  // for(let i=0;i<lowerStr.length;i++){
+  //   if(lowerStr[i]>='a' && lowerStr[i]<='z'){
+  //     newStr+=lowerStr[i];
+  //   }
+  // }
+  // //console.log(newStr);
+  // for(let i=0;i<Math.floor(newStr.length);i++){
+  //   if(newStr[i]!==newStr[newStr.length-1-i]){
+  //     return false;
+  //   }
+  // }
+  // return true;
+  const newStr=str.toLowerCase().replace(/[^a-z]/g,"");
+  return newStr.split("").reverse().join("")===newStr;
 };
 
 // Do not edit below this line
