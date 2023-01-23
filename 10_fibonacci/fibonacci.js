d61bb5c2937a84c96fcda2b5d5b3c29f95db5fd4
webdev <117325718+webdevcodeman@users.noreply.github.com>
2023-01-23 17:06:22 +0530

    Return the fibonacci no. at a specific position>=0

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(num<0){
+    return "OOPS";
+  }
+  if(num==0){
+    return 0;
+  }
+  if(num==1 || num==2){
+    return 1;
+  }
+  let prev=1;
+  let curr=1;
+  let next;
+  for(let i=3;i<=num;i++){
+    next=prev+curr;
+    prev=curr;
+    curr=next;
+  }
+  return next;
 };
 
 // Do not edit below this line
